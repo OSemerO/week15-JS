839f9a3d2377da6c9e4ef1e3f1eaf2cf5c7a42cc
O_Semernina <semernina-olga@mail.ru>
2023-07-05 16:20:12 +0300

    cities and weather

--- week15.js
+++ week15.js
@@ -1,3 +1,9 @@
 let cities = ['Москва', 'Санкт-Петербург','Калининград','Токио','Хельсинки','Таллин','Сеул','Астана','Пекин'];
 let temp = [];
 
+
+for (let i = 0; i < cities.length; i++) {
+  const city = cities[i];
+  const temperature = prompt(`Введите температуру в ${city} (в градусах Цельсия):`);
+  console.log(`Город ${city}: температура ${temperature} градусов Цельсия`);
+}
